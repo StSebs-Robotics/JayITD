799e75c32ed242feb234fe979b7e1a8fd87c6618
amazingjay26 <145387791+amazingjay26@users.noreply.github.com>
2025-02-20 15:26:47 -0500

    wright can toggle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Official.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Official.java
@@ -153,7 +153,35 @@ public class Official extends LinearOpMode {
                 rightFrontDrive.setPower(frontRightPower);
                 rightBackDrive.setPower(backRightPower);
 
-            } else {
+            }
+            else if (currentGamepad1.a) {
+                motorSpeed = Values.speedReducer;
+                double y = -gamepad2.left_stick_y; // Remember, Y stick value is reversed
+                double x = gamepad2.left_stick_x;
+                double rx = gamepad2.right_stick_x;
+                //double botHeading = drive.pose.heading.toDouble();
+                botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+                // Rotate the movement direction counter to the bot's rotation
+                double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+                double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+                rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+                // Denominator is the largest motor power (absolute value) or 1
+                // This ensures all the powers maintain the same ratio,
+                // but only if at least one is out of the range [-1, 1]
+                double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+                double frontLeftPower = (rotY + rotX + rx) / denominator * motorSpeed;
+                double backLeftPower = (rotY - rotX + rx) / denominator * motorSpeed;
+                double frontRightPower = (rotY - rotX - rx) / denominator * motorSpeed;
+                double backRightPower = (rotY + rotX - rx) / denominator * motorSpeed;
+                leftFrontDrive.setPower(frontLeftPower);
+                leftBackDrive.setPower(backLeftPower);
+                rightFrontDrive.setPower(frontRightPower);
+                rightBackDrive.setPower(backRightPower);
+            }
+            else {
                 double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
                 double x = gamepad1.left_stick_x;
                 double rx = gamepad1.right_stick_x;
