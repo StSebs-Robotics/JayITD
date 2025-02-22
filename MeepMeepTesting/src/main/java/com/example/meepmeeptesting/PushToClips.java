6060c46da6c4bfce34718a9edb4f5d5332f8524c
amazingjay26 <145387791+amazingjay26@users.noreply.github.com>
2025-02-21 20:44:44 -0500

    going out path maybe?

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PushToClips.java
@@ -0,0 +1,28 @@
+
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class PushToClips {
+    public static void main(String[] args) {
+        System.setProperty("sun.java2d.opengl", "true");
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new ActualBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(50, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-50,53, Math.toRadians(270)))
+                        .build());
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
